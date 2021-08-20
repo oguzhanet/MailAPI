e438a26b049d76d6f6384ae4263c9f279e52014f
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-08-20 10:40:03 +0300

    Add project files.

---
+++ MailAPI/Controllers/EmailController.cs
@@ -0,0 +1,40 @@
+﻿using MailAPI.Model;
+using MailAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : Controller
+    {
+
+        private readonly IMailService mailService;
+        public EmailController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> Send([FromForm] MailRequest request)
+        {
+            try
+            {
+                await mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+
+        }
+
+
+    }
+}
---
+++ MailAPI/Services/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using MailAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailAPI.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+
+    }
+}
